16c6171ed9a5d5e04e2ce8f64c3320867e01d200
kansoul <76467022+kansoul@users.noreply.github.com>
2021-08-23 19:08:56 +0700

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -46,6 +46,23 @@ async def on_ready():
     }, data=payload)
     print(res.text)
 
+colors = [0xFFE4E1, 0x00FF7F, 0xD8BFD8, 0xDC143C, 0xFF4500, 0xDEB887, 0xADFF2F, 0x800000, 0x4682B4, 0x006400, 0x808080, 0xA0522D, 0xF08080, 0xC71585, 0xFFB6C1, 0x00CED1]
+@client.command(help='Shares a meme')
+async def meme(ctx):
+    subreddit = reddit.subreddit("dankmemes")
+    all_subs = []
+    top = subreddit.top(limit = 75)
+
+    for submission in top:
+      all_subs.append(submission)
+  
+    random_sub = random.choice(all_subs)
+    name = random_sub.title
+    url = random_sub.url
+    em = discord.Embed(title = name, color = random.choice(colors))
+
+    em.set_image(url = url)
+    await ctx.send(embed = em)
 
 @client.event
 async def on_message(message):
